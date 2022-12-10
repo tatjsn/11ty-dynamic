cf9451d3c6bebb0b0dc08963b8169787ac9a6f98
Taz Jackson <tatjsn@users.noreply.github.com>
2022-12-10 10:35:57 +0000

    Add stop view

---
+++ api/stop.js
@@ -0,0 +1,14 @@
+import nunjucks from './shared/nunjucks';
+
+export default async function(req, res) {
+  const reqUrl = new URL(req.url, 'https://example.com');
+  const id = reqUrl.searchParams.get('id');
+  const url = `https://citymapper.com/api/1/departures?headways=1&ids=${id}&region_id=uk-london`;
+  const fetchedRes = await fetch(url);
+  const fetchedData = await fetchedRes.json();
+  const { stops } = fetchedData;
+
+  return res.send(nunjucks().render('stop.njk', {
+    stops,
+  }));
+}
